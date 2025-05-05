6666a0e5d4fee3f7609bf8ae69ea10e01f6fe370
dharmikdhruve009 <d.dhruve@student.uq.edu.au>
2025-05-06 07:54:47 +1000

    Week 7 & 8
    Done with almost everything for Week 7 and Week 8, about to begin with Week 9 the final one

---
+++ work_phase_3/js/behavioural_design.js
@@ -0,0 +1,58 @@
+/**
+* IMPORTS
+* Keep track of external modules being used
+
+* */
+
+/**
+ * CONSTANTS
+ * Define values that don't change e.g. page titles, URLs, etc.
+ * */
+
+/**
+ * VARIABLES
+ * Define values that will change e.g. user inputs, counters, etc.
+ * */
+
+/**
+ * FUNCTIONS
+ * Group code into functions to make it reusable
+ * */
+
+/**
+ * EVENT LISTENERS
+ * The code that runs when a user interacts with the page
+ * */
+
+// In your js/PAGE_NAME.js file
+import postFormData from '.js/modules/postFormData.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('community-form');
+    const feedback = document.getElementById('form-feedback');
+
+    
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        feedback.textContent = 'Submitting...';
+
+        const { success, data } = await postFormData(
+            form,
+            'https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/community/',
+            {
+                student_number: 's4854993',
+                uqcloud_zone_id: '33ba08a0'
+            }
+        );
+
+        if (success) {
+            feedback.textContent = data.message;
+            form.reset();
+        } else {
+            feedback.textContent = data.message || 'Something went wrong.';
+        }
+    });
+});
+
+// when the page fully loads
